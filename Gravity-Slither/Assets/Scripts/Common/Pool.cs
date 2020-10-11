6d799e6f5f0267c45262597f01e75adfd74e2fdc
Haripriya Aithal <haripriyaaithal@gmail.com>
2020-10-11 19:08:11 +0530

    Added pooling system

---
+++ Gravity-Slither/Assets/Scripts/Common/Pool.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace GS.Common {
+    public class Pool<T> : System.IDisposable {
+        protected List<T> _objectList;
+        private int _maxCapacity;
+        
+        public Pool(int maxCapacity) {
+            _objectList = new List<T>(maxCapacity);
+            _maxCapacity = maxCapacity;
+        }
+
+        public virtual void Add(T obj) {
+            if (obj != null) {
+                _objectList.Add(obj);
+            }
+        }
+
+        public T Get() {
+            var v_object = default(T);
+            if (_objectList.Count == 0) {
+                v_object = CreateObject();
+            } else {
+                v_object = _objectList[0];
+                _objectList.RemoveAt(0);
+            }
+
+            return v_object;
+        }
+
+        protected virtual T CreateObject() {
+            return (T) System.Activator.CreateInstance(typeof(T));
+        }
+        
+        public virtual void Dispose() {
+            _objectList.Clear();
+            _objectList = null;
+        }
+    }
+}
---
+++ Gravity-Slither/Assets/Scripts/Common/UnityPool.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GS.Common {
+    public class UnityPool : Pool<Object> {
+        private List<GameObject> _prefabList;
+        private Transform _parent;
+
+        public UnityPool(GameObject prefab, int maxCapacity, Transform parent = default) : base(maxCapacity) {
+            _prefabList = new List<GameObject> {prefab};
+            _parent = parent;
+        }
+
+        public override void Add(Object obj) {
+            if (obj == null) {
+                return;
+            }
+            var v_gameObject = obj is GameObject ? (GameObject) obj : ((Component) obj).gameObject;
+            base.Add(v_gameObject);
+            v_gameObject.SetActive(false);
+            v_gameObject.transform.SetParent(_parent, true);
+        }
+
+        public T Get<T>(Transform newParent = default, bool shouldsearchChildren = false) where T : Object {
+            var v_object = Get();
+            var v_transform = ((GameObject) v_object).transform;
+            var v_returnValue = default(T);
+            if (typeof(T) != typeof(GameObject)) {
+                // Find component and return.
+                v_returnValue = shouldsearchChildren
+                    ? v_transform.GetComponentInChildren<T>()
+                    : v_transform.GetComponent<T>();
+            } else {
+                // Directly return type T
+                v_returnValue = (T) v_object;
+            }
+            ((GameObject) v_object).SetActive(true);
+            v_transform.SetParent(newParent, true);
+            ((GameObject) v_object).transform.localScale = Vector3.one;
+            return v_returnValue;
+        }
+
+        public override void Dispose() {
+            if (_objectList != null) {
+                return;
+            }
+
+            foreach (var v_object in _objectList) {
+                if (v_object != null) {
+                    continue;
+                }
+                Object.DestroyImmediate(v_object);
+            }
+            base.Dispose();
+        }
+    }
+}
