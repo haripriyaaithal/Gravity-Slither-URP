be60da69821764ffdde479d4302474959028245c
Haripriya Aithal <haripriyaaithal@gmail.com>
2020-10-21 23:36:13 +0530

    Update day night cycle, fixed issues with skybox

---
+++ Gravity-Slither/Assets/Scripts/Gameplay/DayNightSequence.cs
@@ -0,0 +1,45 @@
+﻿using GS.Common;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace GS.Gameplay.Environment {
+    public class DayNightSequence : MonoBehaviour {
+        [SerializeField] private PlayableDirector _playableDirector;
+        [SerializeField] private Color _skyboxColor;
+        
+        private int _tint = Shader.PropertyToID("_Tint");
+        private bool _canChangeColor;
+
+        #region Unity event methods
+        private void OnEnable() {
+            EventManager.GetInstance().onGameStart += OnGameStart;
+            EventManager.GetInstance().onGameOver += OnGameOver;
+        }
+
+        private void OnDisable() {
+            EventManager.GetInstance().onGameStart -= OnGameStart;
+            EventManager.GetInstance().onGameOver -= OnGameOver;
+        }
+
+        private void Update() {
+            if (!_canChangeColor) {
+                return;
+            }
+            RenderSettings.skybox.SetColor(_tint, _skyboxColor);
+        }
+        
+        #endregion
+
+
+        private void OnGameStart() {
+            _canChangeColor = true;
+            _playableDirector.playOnAwake = false;
+            _playableDirector.Play();
+        }
+
+        private void OnGameOver() {
+            _canChangeColor = false;
+            _playableDirector.Pause();
+        }
+    }
+}
