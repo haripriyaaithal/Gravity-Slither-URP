ae7c898c4a823edcad3f292b5fe58f6605983b66
Haripriya Aithal <haripriyaaithal@gmail.com>
2020-10-27 10:07:43 +0530

    Added main menu and game play UI

---
+++ Gravity-Slither/Assets/Scripts/Menu/RotateGameObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace GS.Common {
+    public class RotateGameObject : MonoBehaviour {
+        [SerializeField] private float _angle;
+
+        private Transform _transform;
+
+        private void Awake() {
+            _transform = transform;
+        }
+
+        private void Update() {
+            _transform.RotateAround(Vector3.zero, Vector3.up, _angle * Time.deltaTime);
+        }
+    }
+}
