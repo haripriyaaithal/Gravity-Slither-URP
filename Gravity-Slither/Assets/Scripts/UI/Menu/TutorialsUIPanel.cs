2d0f896300b61d907f06cf464405795345eeb586
Haripriya Aithal <haripriyaaithal@gmail.com>
2020-11-13 21:36:16 +0530

    Added next and previous buttons in tutorials

--- Gravity-Slither/Assets/Scripts/UI/Menu/TutorialsUIPanel.cs
+++ Gravity-Slither/Assets/Scripts/UI/Menu/TutorialsUIPanel.cs
@@ -1,16 +1,23 @@
 ﻿using System.Collections.Generic;
 using GS.UI.Common;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GS.UI {
     public class TutorialsUIPanel : PanelBase {
-        [Header("UI Transitions")] 
-        [SerializeField] private AnimationUIElement _panelHeader;
+        [SerializeField] private ScrollRect _scrollRect;
+        [SerializeField] private Button _previousButton;
+        [SerializeField] private Button _nextButton;
+
+        [Header("UI Transitions")] [SerializeField]
+        private AnimationUIElement _panelHeader;
+
         [SerializeField] private AnimationUIElement _panelBody;
         [SerializeField] private CanvasGroup _panelCanvasGroup;
 
         private List<int> _tweenIdList;
-        
+        private float _toScrollValue;
+
         #region UI Transitions
 
         private void AnimateUIOpen() {
@@ -18,7 +25,8 @@ namespace GS.UI {
 
             LeanTween.move(_panelHeader.target, _panelHeader.fromRect.anchoredPosition3D, 0);
             _tweenIdList.Add(LeanTween.move(_panelHeader.target, _panelHeader.endPosition, _panelHeader.time)
-                .setFrom(_panelHeader.fromRect.anchoredPosition3D).setEaseOutExpo().setDelay(_panelHeader.delay).uniqueId);
+                .setFrom(_panelHeader.fromRect.anchoredPosition3D).setEaseOutExpo().setDelay(_panelHeader.delay)
+                .uniqueId);
 
             LeanTween.move(_panelBody.target, _panelBody.fromRect.anchoredPosition3D, 0);
             _tweenIdList.Add(LeanTween.move(_panelBody.target, _panelBody.endPosition, _panelBody.time)
@@ -28,20 +36,32 @@ namespace GS.UI {
 
         private void AnimateUIClose(System.Action callback) {
             _panelCanvasGroup.blocksRaycasts = false;
-            _tweenIdList.Add(LeanTween.move(_panelHeader.target, _panelHeader.fromRect.anchoredPosition3D, _panelHeader.time)
+            _tweenIdList.Add(LeanTween
+                .move(_panelHeader.target, _panelHeader.fromRect.anchoredPosition3D, _panelHeader.time)
                 .setFrom(_panelHeader.target.anchoredPosition3D).setEaseOutExpo().uniqueId);
             _tweenIdList.Add(LeanTween.move(_panelBody.target, _panelBody.fromRect.anchoredPosition3D, _panelBody.time)
                 .setFrom(_panelBody.target.anchoredPosition3D).setEaseOutExpo().setOnComplete(
                     () => { callback?.Invoke(); }).uniqueId);
-            
+
             LeanTween.alphaCanvas(_panelCanvasGroup, 0, 0.1f);
         }
 
+        private void AnimateScroll() {
+            LeanTween.value(gameObject, _scrollRect.horizontalNormalizedPosition, _toScrollValue, 0.3f).setOnUpdate(
+                (value) => {
+                    _scrollRect.horizontalNormalizedPosition = value;
+                    HandleButtons();
+                }).setEaseOutExpo();
+        }
+
         #endregion
-        
+
         #region Panel Stacker implementation
 
         public override void OnPanelOpened() {
+            _scrollRect.horizontalNormalizedPosition = 0f;
+            _toScrollValue = 0;
+            HandleButtons();
             base.OnPanelOpened();
             _tweenIdList = _tweenIdList ?? new List<int>();
             _tweenIdList.ForEach(LeanTween.cancel);
@@ -62,13 +82,20 @@ namespace GS.UI {
         }
 
         public void OnClickNext() {
-            
+            _toScrollValue = Mathf.Clamp(_toScrollValue + (1 / 5f), 0, 1);
+            AnimateScroll();
         }
 
         public void OnClickPrevious() {
-            
+            _toScrollValue = Mathf.Clamp(_toScrollValue - (1 / 5f), 0, 1);
+            AnimateScroll();
         }
 
         #endregion
+
+        private void HandleButtons() {
+            _previousButton.interactable = _scrollRect.horizontalNormalizedPosition > 1 / 6f;
+            _nextButton.interactable = _scrollRect.horizontalNormalizedPosition < 5 / 6f;
+        }
     }
 }
